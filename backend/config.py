972bc548ed020d7858752dd0a25e2037cd72c9c7
mayank1784 <mayankjain1784@gmail.com>
2024-11-15 18:12:59 +0530

    added deployment configs

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,25 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 from datetime import timedelta
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-i-am-from-iitm-diploma'
-    REMEMBER_COOKIE_DURATION = timedelta(days=3)  # Example: 30 days
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-i-am-from-iitm-diploma'  # Fallback if not set
+    REMEMBER_COOKIE_DURATION = timedelta(days=3)  # Can be adjusted as needed
+    
+    # Use DATABASE_URL for production databases (e.g., PostgreSQL, MySQL)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'sentimentScout.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids unnecessary overhead for modification tracking
+    
+    # For Production Logging and Error Handling
+    if os.environ.get('FLASK_ENV') == 'production':
+        DEBUG = False
+        TESTING = False
+        LOGGING_LEVEL = 'ERROR'
+    else:
+        DEBUG = True
+        TESTING = True
+        LOGGING_LEVEL = 'DEBUG'
+
+    
